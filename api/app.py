7d47b032ed433be5c2be1f64bccf6f3487d3521b
abhishek2393 <coopisthebestkick@gmail.com>
2023-08-10 13:47:48 +0530

    vercel test for flask

--- api/app.py
+++ api/app.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 from os import getenv
 
 # BluePrints
-from blueprints.posts import posts
-from blueprints.ideas import ideas
-load_dotenv()
+from blueprints.test import test
+
+
 app = Flask(__name__)
 CORS(app)
-app.register_blueprint(posts, url_prefix='/api/test')
+app.register_blueprint(test, url_prefix='/api/test')
 
 
 if __name__ == '__main__':
--- api/blueprints/test.py
+++ api/blueprints/test.py
@@ -1,13 +1,10 @@
 from pprint import pprint
 from flask import Blueprint, jsonify, request
 
-posts = Blueprint('posts', __name__)
+posts = Blueprint('test', __name__)
 
 
 
 @posts.route('/thoughts', methods=['POST'])
 def thoughts():
-    return jsonify({
-        'success': True,
-        'post': 'thoughts'
-    })
+    return 'Hello World!'
